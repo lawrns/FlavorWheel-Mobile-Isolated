b6c001bbf31c1ea43f55769ba5e1aee388cc10ed
Julian <julian@MacBook-Pro.local>
2025-09-01 07:58:55 -0600

    🎨 Replace Landing BG HD with Landing BG SD background image

--- app/[locale]/landing/page.tsx
+++ app/[locale]/landing/page.tsx
@@ -19,7 +19,7 @@ export default function LandingPage() {
       style={{
         WebkitOverflowScrolling: 'touch',
         overscrollBehavior: 'none',
-        backgroundImage: 'url(/images/Landing%20BG%20HD.png)',
+        backgroundImage: 'url(/images/Landing%20BG%20SD.jpg)',
         minHeight: '100vh'
       }}
     >
